c601b3d20f67f97236dbfb8dc9fe23deef956722
Sarthak Saxena <61883822+sarthak1905@users.noreply.github.com>
2022-01-18 11:13:48 +0530

    Time: 168 ms (14.02%), Space: 51.4 MB (40.00%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); ++i){
             if(mp.find(nums[i]) == mp.end())
-                mp.insert(make_pair(nums[i], 1));
+                mp[nums[i]]++;
             else
                 return true;
         }
