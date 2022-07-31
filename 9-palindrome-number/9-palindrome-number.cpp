819de90d1835c3bc92e9ea3022b1711a07b1532d
Sarthak Saxena <61883822+sarthak1905@users.noreply.github.com>
2022-07-31 17:42:28 +0530

    Time: 28 ms (34.06%), Space: 5.9 MB (31.87%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long int i=x, n=0;
+        while(i>0)
+        {
+            n=i%10+n*10;
+            i/=10;
+        }
+        return n==x;
+    }
+};
